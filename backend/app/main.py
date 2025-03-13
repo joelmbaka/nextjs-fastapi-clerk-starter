b93f11be9cd82a07f734aa8b7fcffdde477470d3
joelmbaka <dev@joelmbaka.site>
2025-03-14 00:05:38 +0300

    nextjs python

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["https://nextjs-fastapi-clerk-starter.vercel.app/"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
